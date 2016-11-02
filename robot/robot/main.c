675cd9b6d92e0077553d3e51a450e595ba30779c
Simone Rossi <simone.rossi.93@gmail.com>
2016-11-02 12:18:39 +0100

    New Xcode project added

---
+++ robot/robot/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  robot
+//
+//  Created by Simone Rossi on 02/11/16.
+//  Copyright © 2016 Simone Rossi. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  printf("Hello, World!\n");
+    return 0;
+}
