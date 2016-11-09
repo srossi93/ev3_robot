4c30c48e1bfbd858330b2cddbf4f1d43a5548f85
Simone Rossi <simone.rossi.93@gmail.com>
2016-11-09 18:37:29 +0100

    Test turn in place

--- robot/src/main.c
+++ robot/src/main.c
@@ -39,9 +39,9 @@ int main( void )
 
   turn_inplace_by_relative_angle(-90, right_engine, left_engine);
   
-  //turn_inplace_by_relative_angle(180, right_engine, left_engine);
+  turn_inplace_by_relative_angle(180, right_engine, left_engine);
   
-  //turn_inplace_by_relative_angle(-180, right_engine, left_engine);
+  turn_inplace_by_relative_angle(-180, right_engine, left_engine);
   
   
   ev3_uninit();
