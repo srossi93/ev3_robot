380043461039d20d39b2a55f8e1f5d738e8bb644
Simone Rossi <simone.rossi.93@gmail.com>
2016-11-09 19:31:13 +0100

    test

--- robot/src/main.c
+++ robot/src/main.c
@@ -25,24 +25,33 @@
 #include "movement.h"
 
 extern char msg[255];
-
+extern sem_t sem_right_engine, sem_left_engine;
 
 int main( void )
 {
   engine_ptr right_engine, left_engine;
+  int sem_r, sem_l;
   
   if (!robot_init()) return 1;
   
   identify_engines(&right_engine, &left_engine);
   
+  sem_getvalue(&sem_right_engine, &sem_r);
+  sem_getvalue(&sem_left_engine, &sem_l);
+  
+  printf("Sem right: %d Sem left: %d", sem_r, sem_l);
   turn_inplace_by_relative_angle(90, right_engine, left_engine);
   sleep(1);
+  printf("Sem right: %d Sem left: %d", sem_r, sem_l);
   turn_inplace_by_relative_angle(-90, right_engine, left_engine);
   sleep(1);
+  printf("Sem right: %d Sem left: %d", sem_r, sem_l);
   turn_inplace_by_relative_angle(180, right_engine, left_engine);
-    sleep(1);
+  sleep(1);
+  printf("Sem right: %d Sem left: %d", sem_r, sem_l);
   turn_inplace_by_relative_angle(-180, right_engine, left_engine);
-  
+  sleep(1);
+  printf("Sem right: %d Sem left: %d", sem_r, sem_l);
   
   ev3_uninit();
   printf( "*** ( EV3 ) Bye! ***\n" );
