3c2bacdca687a7b9e04c2eba73afa41696024bab
Simone Rossi <simone.rossi.93@gmail.com>
2016-11-10 15:06:09 +0100

    Turn in place function - Test completed

---
+++ robot/include/test.h
@@ -0,0 +1,16 @@
+//
+//  test.h
+//  robot
+//
+//  Created by Simone Rossi on 10/11/16.
+//  Copyright © 2016 Simone Rossi. All rights reserved.
+//
+
+#ifndef test_h
+#define test_h
+
+#include "movement.h"
+
+int test_turn_inplace_by_relative_angle();
+
+#endif /* test_h */
---
+++ robot/src/test.c
@@ -0,0 +1,37 @@
+//
+//  test.c
+//  robot
+//
+//  Created by Simone Rossi on 10/11/16.
+//  Copyright © 2016 Simone Rossi. All rights reserved.
+//
+
+#include <test.h>
+
+int test_turn_inplace_by_relative_angle() {
+  
+  engine_ptr right_engine, left_engine;
+  
+  if (!identify_engines(&right_engine, &left_engine)) return 1;
+  
+  int i = 0;
+  for (i = 0; i < 8 ; i++)
+  {
+    turn_inplace_by_relative_angle(45, right_engine, left_engine);
+    turn_inplace_by_relative_angle(-45, right_engine, left_engine);
+  }
+  for (i = 0; i < 4 ; i++)
+  {
+    turn_inplace_by_relative_angle(90, right_engine, left_engine);
+    turn_inplace_by_relative_angle(-90, right_engine, left_engine);
+  }
+  for (i = 0; i < 2 ; i++)
+  {
+    turn_inplace_by_relative_angle(180, right_engine, left_engine);
+    turn_inplace_by_relative_angle(-180, right_engine, left_engine);
+  }
+  turn_inplace_by_relative_angle(360, right_engine, left_engine);
+  turn_inplace_by_relative_angle(-360, right_engine, left_engine);
+  
+  return 0;
+}
